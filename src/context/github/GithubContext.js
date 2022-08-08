583dcff350650260031cfc00b9ed5504c9e6fd35
Apples <junyiappleli@gmail.com>
2022-08-08 02:16:36 -0400

    added more to single profile

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -11,6 +11,7 @@ export const GithubProvider = ({ children }) => {
     users: [],
     user: {},
     loading: false,
+    repos: [],
   };
 
   const [state, dispatch] = useReducer(githubReducer, initalState);
@@ -59,6 +60,32 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+  //Get user's repos
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
+  };
+
   //Set loading
   const setLoading = () => {
     dispatch({
@@ -79,9 +106,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         user: state.user,
         loading: state.loading,
+        repos: state.repos,
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {children}
